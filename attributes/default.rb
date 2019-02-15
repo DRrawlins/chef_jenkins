9f96d409efe3b47544c6c08131f09656f9735548
Daniel Rawlins <danrawlins07@gmail.com>
2019-02-15 12:48:57 -0500

    added in some needed attributes for dynamic modification where needed

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 # Cookbook Name:: chef_jenkins
 
-default['chef-jenkins']['jenkins_service_name'] = 'jenkins'
+default['chef_jenkins']['jenkins_service_name'] = 'jenkins'
+default['chef_jenkins']['master']['JENKINS_JAVA_CMD'] = '-Djenkins.install.runSetupWizard=false'
