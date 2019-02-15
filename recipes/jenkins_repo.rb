6d96eb6240bec90d466bdc1a0f89cfc324e94cdf
Daniel Rawlins <danrawlins07@gmail.com>
2019-02-15 12:54:15 -0500

    added in jenkins repo for rhel based oses

---
+++ recipes/jenkins_repo.rb
@@ -0,0 +1,32 @@
+yum_repository 'jenkins_repo' do
+  name            node['chef_jenkins']['master']['jenkins_repo_package_name'] # ~FC120
+  clean_metadata  true
+  clean_headers  true
+  description     node['chef_jenkins']['master']['jenkins_repo_package_desc']
+  baseurl         node['chef_jenkins']['master']['jenkins_repo_package_base_url']
+  enabled         true
+  gpgcheck        true
+  mode            '0644'
+  action          :create
+  notifies :run, 'execute[rpm_gpg_import_jenkins]', :immediately
+end
+
+execute 'rpm_gpg_import_jenkins' do
+  command "sudo rpm --import #{node['chef_jenkins']['master']['jenkins_gpgkey_url']}"
+  action :nothing
+  notifies :run, 'execute[dnf_repolist]', :immediately
+end
+
+execute 'dnf_repolist' do
+  command 'dnf repolist'
+  action :nothing
+end
+
+remote_file 'jenkins_gpg' do
+  source          node['chef_jenkins']['master']['jenkins_gpgkey_url']
+  path            node['chef_jenkins']['master']['jenkins_gpgkey_path']
+  owner           'root'
+  group           'root'
+  mode            '0644'
+  action          :create
+end
