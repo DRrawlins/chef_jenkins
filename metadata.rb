d4d28e5091684c936c5050dfcc301f35743feb2f
Daniel Rawlins <danrawlins07@gmail.com>
2019-02-15 12:48:28 -0500

    fixed some foodcritic issues with metdata and added some more dependencies in gemfile

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 name 'chef_jenkins'
 maintainer 'Daniel Rawlins'
 maintainer_email 'danrawlins07@gmail.com'
+license          'MIT'
 description 'Installs/Configures chef_jenkins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
@@ -8,5 +9,8 @@ version '0.1.0'
 %w{fedora}.each do |os|
   supports os
 end
+
+source_url 'https://github.com/DRrawlins/chef_jenkins'
+issues_url 'https://github.com/DRrawlins/chef_jenkins/issues'
 
 chef_version '>= 14.10.9'
