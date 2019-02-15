c7997fc29b5188d042d9fe4c3bbc78a9f5e94335
Daniel Rawlins <danrawlins07@gmail.com>
2019-02-15 12:51:46 -0500

    added default recipe that will source other recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,7 @@
 # Cookbook Name:: chef_jenkins
 # Recipe:: default
 
+include_recipe 'chef_jenkins::jenkins_repo'
 include_recipe 'chef_jenkins::install'
 include_recipe 'chef_jenkins::configure'
 include_recipe 'chef_jenkins::service'
