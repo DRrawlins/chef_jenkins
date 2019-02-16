39eda59a3a727780ebaaba86369d6c7580065fa7
Daniel Rawlins <danrawlins07@gmail.com>
2019-02-16 15:04:49 -0500

    changed java version

--- attributes/install.rb
+++ attributes/install.rb
@@ -1,6 +1,6 @@
 default['chef_jenkins']['master']['jenkins_package_version'] = '2.164'
 default['chef_jenkins']['master']['jenkins_package_name'] = 'jenkins'
-default['chef_jenkins']['master']['java_package_version'] = '11.0.2.7'
-default['chef_jenkins']['master']['java_package_name'] = 'java-11-openjdk'
+default['chef_jenkins']['master']['java_package_version'] = '1.8.0.201.b09'
+default['chef_jenkins']['master']['java_package_name'] = 'java-1.8.0-openjdk'
 default['chef_jenkins']['master']['unzip_package_version'] = '6.00'
 default['chef_jenkins']['master']['unzip_package_name'] = 'unzip'
