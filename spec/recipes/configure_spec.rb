974d30537b5b56ad3ad6802458cc6400c993a888
drawlins-gl <drawlins@greenlancer.com>
2019-02-16 15:08:29 -0500

    moved spec file and added different style of testing

---
+++ spec/recipes/configure_spec.rb
@@ -0,0 +1,95 @@
+require 'spec_helper'
+
+describe 'chef_jenkins::configure' do
+  context 'default' do
+    let(:home_dir) { '/var/lib/jenkins' }
+    let(:log_dir)  { '/var/log/jenkins' }
+    let(:user)          { 'jenkins' }
+    let(:group)         { 'jenkins' }
+
+    cached(:chef_run) do
+      ChefSpec::ServerRunner.new do |node|
+        node.normal['chef_jenkins']['master']['home_dir'] = home_dir
+        node.normal['chef_jenkins']['master']['log_dir'] = log_dir
+        node.normal['chef_jenkins']['master']['user']           = user
+        node.normal['chef_jenkins']['master']['group']          = group
+      end.converge(described_recipe)
+    end
+
+    before do
+      allow_any_instance_of(Chef::Recipe).to receive(:include_recipe)
+    end
+
+    it 'creates the user' do
+      expect(chef_run).to create_user(user)
+        .with_home(home_dir)
+    end
+
+    it 'creates the group' do
+      expect(chef_run).to create_group(group)
+        .with_members([user])
+    end
+
+    it 'creates the home directory' do
+      expect(chef_run).to create_directory(home_dir)
+        .with_owner(user)
+        .with_group(group)
+        .with_mode('0755')
+        .with_recursive(true)
+    end
+
+    it 'creates the log directory' do
+      expect(chef_run).to create_directory(log_dir)
+        .with_owner(user)
+        .with_group(group)
+        .with_mode('0755')
+        .with_recursive(true)
+    end
+  end
+
+  context 'non-default' do
+    let(:home_dir)      { '/var/lib/jenkins' }
+    let(:log_dir) { '/var/log/jenkins' }
+    let(:user)          { 'jenkins' }
+    let(:group)         { 'jenkins' }
+
+    cached(:chef_run) do
+      ChefSpec::ServerRunner.new do |node|
+        node.normal['chef_jenkins']['master']['home_dir']       = home_dir
+        node.normal['chef_jenkins']['master']['log_dir']        = log_dir
+        node.normal['chef_jenkins']['master']['user']           = user
+        node.normal['chef_jenkins']['master']['group']          = group
+      end.converge(described_recipe)
+    end
+
+    before do
+      allow_any_instance_of(Chef::Recipe).to receive(:include_recipe)
+    end
+
+    it 'creates the user' do
+      expect(chef_run).to create_user(user)
+        .with_home(home_dir)
+    end
+
+    it 'creates the group' do
+      expect(chef_run).to create_group(group)
+        .with_members([user])
+    end
+
+    it 'creates the home directory' do
+      expect(chef_run).to create_directory(home_dir)
+        .with_owner(user)
+        .with_group(group)
+        .with_mode('0755')
+        .with_recursive(true)
+    end
+
+    it 'creates the log directory' do
+      expect(chef_run).to create_directory(log_dir)
+        .with_owner(user)
+        .with_group(group)
+        .with_mode('0755')
+        .with_recursive(true)
+    end
+  end
+end
