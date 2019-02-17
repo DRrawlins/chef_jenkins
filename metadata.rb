f3582e55f69b20a9fc11b6eaa7122f9259eec0b3
Daniel Rawlins <danrawlins07@gmail.com>
2019-02-17 09:58:39 -0500

    metadata bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'danrawlins07@gmail.com'
 license          'MIT'
 description 'Installs/Configures chef_jenkins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.4.0'
+version '0.4.1'
 
 %w{fedora}.each do |os|
   supports os
